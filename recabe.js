6c40c43b48fae459aeb0988e726f835446d9d7aa
github_username <you@domain.com>
2023-02-19 22:15:30 +0600

    dom recap

--- recabe.js
+++ recabe.js
@@ -11,4 +11,13 @@ friend.style.textAlign = 'center';
 
 
 })
-
+document.getElementById('btn-apply').addEventListener('click',function(){
+    const friendList = document.getElementById('frined-list');
+    const list = document.createElement('div');
+    list.classList.add('friends')
+    list.innerHTML =`
+    <h3 class=friend-name>Friends</h3>
+    <p>Frineds List</p>
+    `
+    friendList.appendChild(list);
+})
